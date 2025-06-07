f45cd458c33784680f90805bae873540734cd339
Project Name <pawarmahendra46@gmail.com>
2025-06-07 20:13:10 +0530

    changes updated 07 june

---
+++ 2months/Javascripts/1index.js
@@ -0,0 +1,24 @@
+
+//show Message
+
+console.log("Welcome to mahendr pawar")
+
+let Name="Mahendra"
+const Addr="pune"
+var city="satara"
+//function
+function addnumbers(a,b)
+{
+    console.log(a+b)
+  //return a+b;
+}
+//Array
+const arr1=["hi","Hello","thanks","bye"]
+//object
+const obj1={
+     Name:"sandeep",
+    Address:"Ajay" 
+
+}
+//string
+const firststring="Mahendra you are the best1"
---
+++ 2months/Javascripts/2index.js
@@ -0,0 +1,13 @@
+
+const firstname="Mahendra"
+const middlename="hambirrao"
+const lastname="pawar"
+
+function calculteTotal(firstname,middlename,lastname)
+{
+  const fullname= firstname+middlename+lastname
+  console.log(fullname)
+
+}
+
+console.log(calculteTotal(firstname,middlename,lastname))
